c54cae0ccbf0f25c782d1cbf53a9220645f09c55
Himanshu Balani <85930567+himanshubalani@users.noreply.github.com>
2026-04-19 23:56:53 +0530

    complete lab 8

--- src/08-paan-shop.js
+++ src/08-paan-shop.js
@@ -46,17 +46,28 @@
  *   updatePrices({meetha:30, saada:20}, 10)              // => {meetha:40, saada:30}
  */
 export function createPaanOrder(basePaan, customizations) {
-  // Your code here
+  if ( typeof basePaan != "object" || basePaan === null ) return {};
+  let ep = Object.assign({}, basePaan)
+  if (typeof customizations != "object") return ep
+  return Object.assign({}, basePaan, customizations)
 }
 
 export function freezeMenu(menu) {
-  // Your code here
+  if ( typeof menu != "object" || menu === null ) return {};
+  return Object.freeze(menu)
 }
 
 export function updatePrices(menu, increase) {
-  // Your code here
+  if (  typeof menu != "object" || menu === null || typeof increase != "number") return {};
+  const menuArr = Object.entries(menu);
+  const menuMap = new Map();
+
+  for (const [item, price] of menuArr) {
+    menuMap.set(item, price+increase);
+  }
+   return Object.fromEntries(menuMap);
 }
 
 export function mergeDailySpecials(regularMenu, specialsMenu) {
-  // Your code here
+  
 }
